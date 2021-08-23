77b49178e5f12d993f90f1c9c94bc277b2ce2e4c
Saul Uriel <sauluriel1402@gmail.com>
2021-08-22 20:10:00 -0600

    last commit

--- db_config.py
+++ db_config.py
@@ -6,6 +6,8 @@ DB_USER = os.getenv('DB_USER')
 DB_PASSWORD = os.getenv('DB_PASSWORD')
 DB_NAME = os.getenv('DB_NAME')
 
+print(DB_NAME)
+
 client = pymongo.MongoClient(f"mongodb+srv://{DB_USER}:{DB_PASSWORD}@clusterexample.zqc1z.mongodb.net/{DB_NAME}?retryWrites=true&w=majority&ssl=true&ssl_cert_reqs=CERT_NONE")
 
 # This is the db name
